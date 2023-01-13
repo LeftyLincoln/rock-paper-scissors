88814630d8e0642c73f7f24bd8231c187bc6bc16
Travis Parks <travisparks0788@gmail.com>
2023-01-13 10:15:07 -0500

    Add starting event listeners and handlers

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 //Global Variables
 
+var displayAsset = {
+  '📄' : 'assets/happy-paper.png',
+  '🪨' : 'assets/happy-rocks.png',
+  '✄' : 'assets/lines-scissors.png',
+  '🦎': 'assets/lizard.png',
+  '👽' : 'assets/ufo.png'
+}
 
 
 //DOM Variables
@@ -7,6 +14,7 @@ userSection = document.getElementById('userSection')
 userCounter = document.getElementById('userCounter')
 changeGameButton = document.getElementById('changeGameButton')
 beforeGameHeader = document.getElementById('beforeGameHeader')
+duringGameHeader = document.getElementById('duringGameHeader')
 bothRules = document.getElementById('bothRules')
 classicSection = document.getElementById('classic')
 difficultSection = document.getElementById('difficult')
@@ -15,28 +23,41 @@ computerSection = document.getElementById('computerSection')
 computerCounter = document.getElementById('computerCounter')
 
 //Event Listeners
-
-
+classicSection.addEventListener('click', loadClassicGame)
+difficultSection.addEventListener('click', loadDifficultGame)
+changeGameButton.addEventListener('click', changeGame)
 
 //Event Handlers
+
+function loadClassicGame() {
+hide(classic)
+hide(difficult)
+hide(beforeGameHeader)
+show(changeGameButton)
+show(duringGameHeader)
+}
+
+function loadDifficultGame() {
+hide(classic)
+hide(difficult)
+show(changeGameButton)
+hide(beforeGameHeader)
+show(duringGameHeader)
+}
+
+function changeGame() {
+show(classic)
+show(difficult)
+show(beforeGameHeader)
+hide(duringGameHeader)
+hide(changeGameButton)
+}
+
 
 
 
 
 //Functions
-
-
-
-
-
-
-
-
-
-
-
-
-
 function hide(element) {
   element.classList.add('hidden')
 }
