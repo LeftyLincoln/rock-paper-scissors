075950d04ff01490031d34d1134bb0dda01ed671
Travis Parks <travisparks0788@gmail.com>
2023-01-12 19:27:34 -0500

    Add DOM variables

--- main.js
+++ main.js
@@ -3,8 +3,16 @@
 
 
 //DOM Variables
-
-
+userSection = document.getElementById('userSection')
+userCounter = document.getElementById('userCounter')
+changeGameButton = document.getElementById('changeGameButton')
+beforeGameHeader = document.getElementById('beforeGameHeader')
+bothRules = document.getElementById('bothRules')
+classicSection = document.getElementById('classic')
+difficultSection = document.getElementById('difficult')
+fightSection = document.getElementById('fightSection')
+computerSection = document.getElementById('computerSection')
+computerCounter = document.getElementById('computerCounter')
 
 //Event Listeners
 
